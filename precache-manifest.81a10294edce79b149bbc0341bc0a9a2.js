799793713ba7a38c8c59aa0e0c0e627146c49a56
devilla <dev.koold@gmail.com>
2021-07-15 12:10:58 +0530

    Updates

---
+++ precache-manifest.81a10294edce79b149bbc0341bc0a9a2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d54ac8d0d3c917b48f12",
+    "url": "/EthSwap/static/css/main.43debda4.chunk.css"
+  },
+  {
+    "revision": "d54ac8d0d3c917b48f12",
+    "url": "/EthSwap/static/js/main.d54ac8d0.chunk.js"
+  },
+  {
+    "revision": "32526b08b54d4c5ca732",
+    "url": "/EthSwap/static/css/1.f8804169.chunk.css"
+  },
+  {
+    "revision": "32526b08b54d4c5ca732",
+    "url": "/EthSwap/static/js/1.32526b08.chunk.js"
+  },
+  {
+    "revision": "23f7ea5a753bbd83b86f",
+    "url": "/EthSwap/static/js/runtime~main.23f7ea5a.js"
+  },
+  {
+    "revision": "2565884a03843bf5f37bf4a806402acf",
+    "url": "/EthSwap/static/media/avax-logo.2565884a.png"
+  },
+  {
+    "revision": "8059359a1c972b1899ac4695437f0971",
+    "url": "/EthSwap/static/media/token-logo.8059359a.png"
+  },
+  {
+    "revision": "26d4501f9d3f49e0a1fa9e86cd462de3",
+    "url": "/EthSwap/static/media/dai-logo.26d4501f.png"
+  },
+  {
+    "revision": "187cdcc72ee21aa64d50a8b83c53993c",
+    "url": "/EthSwap/static/media/chainlink-link-logo.187cdcc7.png"
+  },
+  {
+    "revision": "d8b67a24cc82134a2e9524a7484842e5",
+    "url": "/EthSwap/index.html"
+  }
+];
